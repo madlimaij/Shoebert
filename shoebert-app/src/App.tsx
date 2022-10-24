081ef4f67054aa5fef199edca3f067f836e3e4b8
Madli-Mai Jõe <madlimai.joergmail.com>
2022-10-24 10:06:54 +0300

    Lisasin nupu

--- shoebert-app/src/App.tsx
+++ shoebert-app/src/App.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Kingapoe nupp!</div>;
+  return (
+    <div className="App">
+      Tere tulemast maailma, Shoebert/Niperbond! <br />
+      <button onClick={() => alert("Jalanõuikaldus, tule hiljem tagasi!")}>
+        Sisene siit
+      </button>
+    </div>
+  );
 }
 
 export default App;
