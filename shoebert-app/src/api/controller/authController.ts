88814a6758ffd77fe202de1e7245bdad21337ef4
Hanna Reinkort <reinkort@gmail.com>
2022-11-01 13:54:34 +0200

    muudatused 01112022

--- shoebert-app/src/api/controller/authController.ts
+++ shoebert-app/src/api/controller/authController.ts
@@ -25,6 +25,28 @@ export async function registerUser(user: any): Promise<any> {
  
 }
 
+export async function loginUser(user: any): Promise<any> {
+    const response = await fetch(getPath(ApiPath.Auth.login), {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    if (response.status === 200) {
+        const data = await response.json();
+
+        return {
+            body: data, 
+            isSuccess: true,
+        }
+    }
+    return {
+        isSuccess: false,
+    }
+}
+
+
 export async function getCurrentUser(): Promise<any> {
     const response = await fetch(getPath(ApiPath.Auth.currentUser), {
         method: "GET",
